e27d9b1600a13a4be18780ca9cb189a73c061f98
Brandon Dixon <brandon.dixon@sevun.com>
2018-03-16 15:08:59 -0500

    setup hello world for software folder

---
+++ software/main.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+/*
+ * main.c
+ */
+int main(void) {
+	
+	return 0;
+}
