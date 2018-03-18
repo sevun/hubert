faac246cad2e340f6228d152c398b84758b7d478
Brandon Dixon <brandon.dixon@sevun.com>
2018-03-18 14:59:21 -0500

    updated include files to only is minimum files

--- software/main.c
+++ software/main.c
@@ -19,13 +19,10 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "inc/hw_gpio.h"
 #include "inc/hw_ints.h"
 #include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
-#include "driverlib/pin_map.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/timer.h"
 #include "pinmux.h"
