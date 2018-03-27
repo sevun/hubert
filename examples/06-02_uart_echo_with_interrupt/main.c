017ad8313f81a7b563ca77d01ebbc4448107adeb
Brandon Dixon <brandon.dixon@sevun.com>
2018-03-27 14:59:49 -0500

    setup echo with interrupt example

---
+++ examples/06-02_uart_echo_with_interrupt/main.c
@@ -0,0 +1,102 @@
+/******************************************************************
+ * Hubert Data Logger
+ * UART0 Echo with Interrupts Example
+ * Developed by Sevun Scientific, Inc.
+ * http://sevunscientific.com
+ * *****************************************************************
+ *
+ *    _____/\\\\\\\\\\\_______/\\\\\\\\\\\____/\\\\\\\\\\\_
+ *     ___/\\\/////////\\\___/\\\/////////\\\_\/////\\\///__
+ *      __\//\\\______\///___\//\\\______\///______\/\\\_____
+ *       ___\////\\\___________\////\\\_____________\/\\\_____
+ *        ______\////\\\___________\////\\\__________\/\\\_____
+ *         _________\////\\\___________\////\\\_______\/\\\_____
+ *          __/\\\______\//\\\___/\\\______\//\\\______\/\\\_____
+ *           _\///\\\\\\\\\\\/___\///\\\\\\\\\\\/____/\\\\\\\\\\\_
+ *            ___\///////////_______\///////////_____\///////////__
+ *
+ * *****************************************************************
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+
+#define UART_SPEED 115200
+
+//*****************************************************************************
+// UART Interrupt Functions
+//*****************************************************************************
+
+volatile bool g_bKeyboardInputFlag;
+
+// The interrupt handler for UART0
+void UART0IntHandler(void)
+{
+    uint32_t ui32Status;
+
+    ui32Status = UARTIntStatus(UART0_BASE, true); //get interrupt status
+
+    UARTIntClear(UART0_BASE, ui32Status); //clear the asserted interrupts
+
+    g_bKeyboardInputFlag = 1;
+}
+
+int main(void)
+{
+    // Set microcontroller to use the 16 MHz external crystal
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+    //*****************************************************************************
+    // Pins Setup
+    //*****************************************************************************
+
+    // Enable the Port A and UART0 peripheral
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    // Sets the pins associated with UART0
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    //*****************************************************************************
+    // UART Setup
+    //*****************************************************************************
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), UART_SPEED,
+        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+    //*****************************************************************************
+    // Configure Interrupts
+    //*****************************************************************************
+
+    // Enable processor interrupts.
+    IntMasterEnable(); //enable processor interrupts
+
+    IntEnable(INT_UART0); //enable the UART interrupt
+    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT); //only enable RX and TX interrupts
+
+    //*****************************************************************************
+    // Main Code
+    //*****************************************************************************
+
+    while (1)
+    {
+        // Timer 0
+        if ( 1 == g_bKeyboardInputFlag )
+        {
+            g_bKeyboardInputFlag = 0;  // Clear the flag for keyboard input interrupt
+
+            while( UARTCharsAvail(UART0_BASE) ) //loop while there are chars
+            {
+                UARTCharPutNonBlocking(UART0_BASE, UARTCharGetNonBlocking(UART0_BASE)); //echo character
+            }
+        }
+    }
+}
