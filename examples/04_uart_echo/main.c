7e8f485e20c2b73404e2e1df132b604efffca221
Brandon Dixon <brandon.dixon@sevun.com>
2018-04-21 19:57:19 -0500

    put uart echo example back after accidental deletion

---
+++ examples/04_uart_echo/main.c
@@ -0,0 +1,58 @@
+/******************************************************************
+ * Hubert Data Logger
+ * UART0 Echo Example
+ * Developed by Sevun Scientific, Inc.
+ * http://sevunscientific.com
+ * *****************************************************************
+ *
+ *    _____/\\\\\\\\\\\_______/\\\\\\\\\\\____/\\\\\\\\\\\_
+ *     ___/\\\/////////\\\___/\\\/////////\\\_\/////\\\///__
+ *      __\//\\\______\///___\//\\\______\///______\/\\\_____
+ *       ___\////\\\___________\////\\\_____________\/\\\_____
+ *        ______\////\\\___________\////\\\__________\/\\\_____
+ *         _________\////\\\___________\////\\\_______\/\\\_____
+ *          __/\\\______\//\\\___/\\\______\//\\\______\/\\\_____
+ *           _\///\\\\\\\\\\\/___\///\\\\\\\\\\\/____/\\\\\\\\\\\_
+ *            ___\///////////_______\///////////_____\///////////__
+ *
+ * *****************************************************************
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+
+int main(void)
+{
+    // Set microcontroller to use the 16 MHz external crystal
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+    // Enable the Port A and UART0 peripheral
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    // Sets the pins associated with UART0
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    // Configure the UART for 115,200, 8-N-1 operation.
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+                            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                             UART_CONFIG_PAR_NONE));
+
+    // Runs this code repeatedly (called polling)
+    while(1)
+    {
+        // Check to see if there is an incoming character on the UART0 Rx
+        if ( UARTCharsAvail(UART0_BASE) )
+        {
+            // Gets the character the echoes it back to UART0 Tx
+            UARTCharPutNonBlocking(UART0_BASE, UARTCharGetNonBlocking(UART0_BASE));
+        }
+    }
+}
